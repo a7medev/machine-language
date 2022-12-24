73b2cbf6f90279ac53c2fee51333e9a5ee5fe0ba
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-12-24 18:23:01 +0200

    chore: use custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: 'auto minmax(500px, 1fr) auto',
+      },
+      fontFamily: {
+        sans: [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+    },
   },
   plugins: [],
 };
