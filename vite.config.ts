755b9da577c11608bad2878a73d3ba4570fa0b17
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2022-12-31 22:52:19 +0200

    feat: add PWA and installability support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Machine Language',
+        short_name: 'Machine Language',
+        description:
+          'Simulator for a simple machine language instruction set described in the book "Computer Science: An Overview 13th edition"',
+        start_url: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#101827',
+        background_color: '#101827',
+        display: 'standalone',
+      },
+    }),
+  ],
+});
